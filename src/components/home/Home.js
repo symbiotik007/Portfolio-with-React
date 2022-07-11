b3d5ec29d4e7602dccadca0acd8880e383f7926a
Oscar Bolanos <69918011+symbiotik007@users.noreply.github.com>
2022-07-10 23:41:29 -0500

    update
    update

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -12,7 +12,7 @@ export default function Home() {
    return (
       <Box component={'main'} display={'flex'} flexDirection={{xs: 'column', md: 'row'}} alignItems={'center'}
            justifyContent={'center'} minHeight={'calc(100vh - 175px)'}>
-         <Box className={classNames(Style.avatar, Style.shadowed)} style={{background:  info.gradient}} component={'img'} src={me} width={{xs: '35vh', md: '40vh'}}
+         <Box className={classNames(Style.avatar, Style.shadowed)} style={{background: info.gradient}} component={'img'} src={me} width={{xs: '35vh', md: '40vh'}}
               height={{xs: '35vh', md: '40vh'}}
               borderRadius={'50%'} p={'0.75rem'} mb={{xs: '1rem', sm: 0}} mr={{xs: 0, md: '2rem'}}/>
          <Box>
